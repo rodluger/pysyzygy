6fa9bb549dd584757d8fbd00039304e84f6d9d1a
Rodrigo Luger <rodluger@gmail.com>
2016-05-15 14:39:26 -0700

    docs update

--- examples/transit_plot.py
+++ examples/transit_plot.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-transit_plot.py
----------------
+:py:mod:`transit_plot.py` - A quick light curve
+-----------------------------------------------
 
 Plots a simple light curve using the :py:func:`pysyzygy.PlotTransit`
 function, which also shows the orbit, the limb-darkened stellar disk,
 and a table with all the planet parameters.
 
-.. figure:: ../img/transit_plot.png
+.. figure:: ../pysyzygy/img/transit_plot.png
     :width: 600px
     :align: center
     :height: 100px
@@ -22,14 +22,16 @@ import pysyzygy as ps
 import numpy as np
 import matplotlib.pyplot as pl
 
-# Transit model kwargs
-kwargs = dict(RpRs = 0.1, b = 0.5, 
-              ecc = 0.5, w = 0.,
-              MpMs = 0., rhos = 1.4,
-              per = 5., t0 = 0.,
-              q1 = 0.45, q2 = 0.3,
-              maxpts = 20000,
-              exptime = ps.KEPLONGEXP)
+if __name__ == '__main__':
 
-fig = ps.PlotTransit(**kwargs)
-pl.show()
+  # Transit model kwargs
+  kwargs = dict(RpRs = 0.1, b = 0.5, 
+                ecc = 0.5, w = 0.,
+                MpMs = 0., rhos = 1.4,
+                per = 5., t0 = 0.,
+                q1 = 0.45, q2 = 0.3,
+                maxpts = 20000,
+                exptime = ps.KEPLONGEXP)
+
+  fig = ps.PlotTransit(**kwargs)
+  pl.show()
